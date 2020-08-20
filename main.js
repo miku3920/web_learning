d59e436d509e62b00956739d4346635c45996c5e
miku3920 <45704185+miku3920@users.noreply.github.com>
2020-08-20 22:44:21 +0800

    2-21 css

--- main.js
+++ main.js
@@ -57,16 +57,23 @@
 // });
 
 // 2-19 without jQuery
-window.onload = function () {
-	document.querySelector('#btn1').addEventListener('click', function () {
-		var sel = document.querySelector('#sell');
-		var opts = sel.getElementsByTagName('option');
-		var msg = 'selected: ';
-		for (var i = 0; i < opts.length; i++){
-			if (opts[i].selected) {
-				msg += opts[i].value + ' ';
-			}
-		}
-		document.querySelector('#msg').textContent = msg;
+// window.onload = function () {
+// 	document.querySelector('#btn1').addEventListener('click', function () {
+// 		var sel = document.querySelector('#sell');
+// 		var opts = sel.getElementsByTagName('option');
+// 		var msg = 'selected: ';
+// 		for (var i = 0; i < opts.length; i++){
+// 			if (opts[i].selected) {
+// 				msg += opts[i].value + ' ';
+// 			}
+// 		}
+// 		document.querySelector('#msg').textContent = msg;
+// 	});
+// }
+
+// 2-21 css
+$(function () {
+	$('#btn1').click(function () {
+		$('li').css('color', 'white').css('background','red');
 	});
-}
+});
