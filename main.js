4a3312fbbe5dd316d0bcca75649b7a3280f848fb
miku3920 <45704185+miku3920@users.noreply.github.com>
2020-08-23 19:10:45 +0800

    2-39 hide & show

--- main.js
+++ main.js
@@ -137,15 +137,28 @@
 // });
 
 // 2-37 Ajax getJSON
+// $(function () {
+// 	$('#btn1').click(function () {
+// 		var id = $('#text1').val();
+// 		$.getJSON("data.php", { 'id': id }, function (response, status, xhr) {
+// 			var ol = $('<ul></ul>');
+// 			ol.append('<li>姓名：' + response.name + '</li>');
+// 			ol.append('<li>電郵信箱：' + response.mail + '</li>');
+// 			ol.append('<li>電話號碼：' + response.tel + '</li>');
+// 			$('#msg').empty().append(ol);
+// 		});
+// 	});
+// });
+
+// 2-39 hide & show
 $(function () {
+	var flg = false;
 	$('#btn1').click(function () {
-		var id = $('#text1').val();
-		$.getJSON("data.php", { 'id': id }, function (response, status, xhr) {
-			var ol = $('<ul></ul>');
-			ol.append('<li>姓名：' + response.name + '</li>');
-			ol.append('<li>電郵信箱：' + response.mail + '</li>');
-			ol.append('<li>電話號碼：' + response.tel + '</li>');
-			$('#msg').empty().append(ol);
-		});
+		if (flg) {
+			$('#image').show(500);
+		} else {
+			$('#image').hide(500);
+		}
+		flg = !flg;
 	});
 });
