5a9176d3802cfc3b34091e67df2e5b77ba0fb106
miku3920 <45704185+miku3920@users.noreply.github.com>
2020-08-26 17:45:07 +0800

    3-10 tuple

--- hello.js
+++ hello.js
@@ -1,23 +1,9 @@
-var Season;
-(function (Season) {
-    Season[Season["spring"] = 0] = "spring";
-    Season[Season["summer"] = 1] = "summer";
-    Season[Season["autumn"] = 2] = "autumn";
-    Season[Season["winter"] = 3] = "winter";
-})(Season || (Season = {}));
-var s = Season.summer;
-switch (+s) {
-    case Season.spring:
-        document.write('<h1>Spring</h1>');
-        break;
-    case Season.summer:
-        document.write('<h1>Summer</h1>');
-        break;
-    case Season.autumn:
-        document.write('<h1>Autumn</h1>');
-        break;
-    case Season.winter:
-        document.write('<h1>Winter</h1>');
-        break;
-}
-document.write('<p>此頁面會顯示目前季節。</p>');
+var gender;
+(function (gender) {
+    gender[gender["male"] = 0] = "male";
+    gender[gender["female"] = 1] = "female";
+})(gender || (gender = {}));
+var person;
+person = ['taro', 'taro@yamada', gender.male, 23];
+document.write('<h1>Tuple</h1>');
+document.write('<p>' + person + '</p>');
--- hello.ts
+++ hello.ts
@@ -1,20 +1,10 @@
-enum Season { spring, summer, autumn, winter }
+enum gender { male, female }
+type myname = string
+type mail = string
+type age = number
 
-let s: Season = Season.summer
+let person: [myname, mail, gender, age];
+person = ['taro', 'taro@yamada', gender.male, 23]
 
-switch (+s) {
-	case Season.spring:
-		document.write('<h1>Spring</h1>')
-		break
-	case Season.summer:
-		document.write('<h1>Summer</h1>')
-		break
-	case Season.autumn:
-		document.write('<h1>Autumn</h1>')
-		break
-	case Season.winter:
-		document.write('<h1>Winter</h1>')
-		break
-}
-
-document.write('<p>此頁面會顯示目前季節。</p>')
+document.write('<h1>Tuple</h1>')
+document.write('<p>' + person + '</p>')
