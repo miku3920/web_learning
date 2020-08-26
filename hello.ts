8a39f1bf95c87a6beb81265db4119ad1abbf4c1c
miku3920 <45704185+miku3920@users.noreply.github.com>
2020-08-26 18:55:01 +0800

    3-11 tuple & array

--- hello.ts
+++ hello.ts
@@ -3,8 +3,14 @@ type myname = string
 type mail = string
 type age = number
 
-let person: [myname, mail, gender, age];
-person = ['taro', 'taro@yamada', gender.male, 23]
+type person = [myname, mail, gender, age]
+
+let data: person[] = []
+data.push(['taro', 'taro@yamada', gender.male, 23])
+data.push(['hanako', 'hanako@flower', gender.female, 45])
+data.push(['sachiko', 'sachiko@happy', gender.female, 67])
 
 document.write('<h1>Tuple</h1>')
-document.write('<p>' + person + '</p>')
+document.write('<p>' + data[0] + '</p>')
+document.write('<p>' + data[1] + '</p>')
+document.write('<p>' + data[2] + '</p>')
