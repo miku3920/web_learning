d7e6a5bbf794d4d4ae9c988ddb83de2a45972d72
miku3920 <45704185+miku3920@users.noreply.github.com>
2020-08-26 20:12:55 +0800

    3-13 function

--- hello.js
+++ hello.js
@@ -1,13 +1,8 @@
-var gender;
-(function (gender) {
-    gender[gender["male"] = 0] = "male";
-    gender[gender["female"] = 1] = "female";
-})(gender || (gender = {}));
-var data = [];
-data.push(['taro', 'taro@yamada', gender.male, 23]);
-data.push(['hanako', 'hanako@flower', gender.female, 45]);
-data.push(['sachiko', 'sachiko@happy', gender.female, 67]);
-document.write('<h1>Tuple</h1>');
-document.write('<p>' + data[0] + '</p>');
-document.write('<p>' + data[1] + '</p>');
-document.write('<p>' + data[2] + '</p>');
+function calc(price, tax) {
+    return Math.floor(price * (1.0 + tax));
+}
+function doClick() {
+    var text1 = document.querySelector('#text1');
+    var msg = document.querySelector('#msg');
+    msg.innerHTML = calc(+text1.value, 0.05) + ' 元';
+}
--- hello.ts
+++ hello.ts
@@ -1,16 +1,9 @@
-enum gender { male, female }
-type myname = string
-type mail = string
-type age = number
+function calc(price: number, tax: number): number {
+	return Math.floor(price * (1.0 + tax))
+}
 
-type person = [myname, mail, gender, age]
-
-let data: person[] = []
-data.push(['taro', 'taro@yamada', gender.male, 23])
-data.push(['hanako', 'hanako@flower', gender.female, 45])
-data.push(['sachiko', 'sachiko@happy', gender.female, 67])
-
-document.write('<h1>Tuple</h1>')
-document.write('<p>' + data[0] + '</p>')
-document.write('<p>' + data[1] + '</p>')
-document.write('<p>' + data[2] + '</p>')
+function doClick(): void {
+	let text1: HTMLInputElement = document.querySelector('#text1')
+	let msg: Element = document.querySelector('#msg')
+	msg.innerHTML = calc(+text1.value, 0.05) + ' 元'
+}
