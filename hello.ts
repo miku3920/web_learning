8b739b8a4c311c2e0d2f8af6871ca647d4df0e8f
miku3920 <papa964204@gmail.com>
2020-08-28 20:32:29 +0800

    this is Outer Environment !!
    Outer Environment
    如果要呼叫一個在該 exection context 中沒有的變項時，它會往它的 outer environment 去找
    
    結論
    只有 f 有 this 變項

--- hello.ts
+++ hello.ts
@@ -73,18 +73,21 @@ o.a()
 // f 中的 af 中的 af 中的 this 會指向 f 所在的物件
 // f 中的 af 中的 f 中的 this 會指向 window
 
-// 結論
-// f 會改變 this
-// o, af 不會
-
 // 也就是說
 // 在 o 建立 f，f 內的 this 會變成 o
 // 在 f 建立 f，f 內的 this 會變成 window
 // 在 o 建立 af，af 內的 this 與 o 的 this 相同
 // 在 o 建立 oo，oo 內的 this 與 o 的 this 相同
 
+// Outer Environment 的概念
+// 如果要呼叫一個在該 exection context 中沒有的變項時，它會往它的 outer environment 去找
+
 // 所以
 // 要知道 this 是什麼就往外層找 f
 // this 就是 f 所在的物件 
 // f 在 f or af 裡
 // this 就是 window
+
+// 結論
+// f 有 this 變項
+// o, af 沒有
