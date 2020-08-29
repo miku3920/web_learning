bf94a35e7dab471317f5a5d167ed77a2abcc5d5a
miku3920 <papa964204@gmail.com>
2020-08-29 18:27:18 +0800

    3-20 accessor
    This reverts commit 811a83747d9237c907ee7ad06bba2a50081c3d2b.

--- hello.js
+++ hello.js
@@ -1,91 +1,40 @@
-var _this = this;
-var o = {
-    a: function () {
-        console.log(_this); // window
-        // all this are window
-        var oo = {
-            that: _this,
-            a: function () {
-                console.log(_this);
-                var f = function () {
-                    console.log(this);
-                };
-                f();
-            },
-            f: function () {
-                console.log(this); // oo
-            }
-        };
-        console.log(oo.that);
-        oo.a();
-        oo.f();
-    },
-    f: function () {
-        var _this = this;
-        console.log(this); // o
-        var a = function () {
-            console.log(_this); // o
-            // all arrow this are o
-        };
-        var f = function () {
-            console.log(this); // window
-            // all this are window
-        };
-        var fo = {
-            a: function () {
-                console.log(_this); // o
-                var a = function () {
-                    console.log(_this); // o
-                    // all arrow this are o
-                };
-                var f = function () {
-                    console.log(this); // window
-                    // all this are window
-                };
-                a();
-                f();
-            },
-            f: function () {
-                var _this = this;
-                console.log(this); // fo
-                var a = function () {
-                    console.log(_this); // fo
-                    // all arrow this are fo
-                };
-                var f = function () {
-                    console.log(this); // window
-                    // all this are window
-                };
-                a();
-                f();
-            },
-            fo4: true
-        };
-        a();
-        f();
-        fo.a();
-        fo.f();
+var Person = /** @class */ (function () {
+    function Person(n, y) {
+        this._name = n;
+        this._age = y;
     }
-};
-o.a();
-// f 中的 this 會指向 f 所在的物件
-// af 中的 this 會指向 af 所在物件的外層物件
-// f 中的 f 中的 this 會指向 window
-// f 中的 af 中的 this 會指向 f 所在的物件
-// f 中的 af 中的 af 中的 this 會指向 f 所在的物件
-// f 中的 af 中的 f 中的 this 會指向 window
-// 也就是說
-// 在 o 建立 f，f 內的 this 會變成 o
-// 在 f 建立 f，f 內的 this 會變成 window
-// 在 o 建立 af，af 內的 this 與 o 的 this 相同
-// 在 o 建立 oo，oo 內的 this 與 o 的 this 相同
-// Outer Environment 的概念
-// 如果要呼叫一個在該 exection context 中沒有的變項時，它會往它的 outer environment 去找
-// 所以
-// 要知道 this 是什麼就往外層找 f
-// this 就是 f 所在的物件 
-// f 在 f or af 裡
-// this 就是 window
-// 結論
-// f 有 this 變項
-// o, af 沒有
+    Object.defineProperty(Person.prototype, "age", {
+        get: function () {
+            return this._age;
+        },
+        set: function (y) {
+            this._age = y < 0 ? 0 : y;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Person.prototype, "name", {
+        get: function () {
+            return this._name;
+        },
+        set: function (y) {
+            this._name = y || this._name;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Person.prototype.print = function () {
+        var msg = '<p>My name is ' + this._name + '. I am ' + this.age + ' year old.</p>';
+        return msg;
+    };
+    return Person;
+}());
+function doClick() {
+    var val = document.querySelector('#text1').value;
+    var arr = val.split(',');
+    var obj = new Person(arr[0], parseInt(arr[1]));
+    var msg = document.querySelector('#msg');
+    obj.age = -1;
+    obj.name = "";
+    msg.innerHTML = obj.print();
+}
--- hello.ts
+++ hello.ts
@@ -1,93 +1,41 @@
-var o = {
-	a: () => {
-		console.log(this) // window
-		// all this are window
-		var oo = {
-			that: this,
-			a: () => {
-				console.log(this)
-				var f = function () {
-					console.log(this)
-				}
-				f()
-			},
-			f: function () {
-				console.log(this) // oo
-			}
-		}
-		console.log(oo.that)
-		oo.a()
-		oo.f()
-	},
-	f: function () {
-		console.log(this) // o
-		var a = () => {
-			console.log(this) // o
-			// all arrow this are o
-		}
-		var f = function () {
-			console.log(this) // window
-			// all this are window
-		}
-		var fo = {
-			a: () => {
-				console.log(this) // o
-				var a = () => {
-					console.log(this) // o
-					// all arrow this are o
-				}
-				var f = function () {
-					console.log(this) // window
-					// all this are window
-				}
-				a()
-				f()
-			},
-			f: function () {
-				console.log(this) // fo
-				var a = () => {
-					console.log(this) // fo
-					// all arrow this are fo
-				}
-				var f = function () {
-					console.log(this) // window
-					// all this are window
-				}
-				a()
-				f()
-			},
-			fo4: true
-		}
-		a()
-		f()
-		fo.a()
-		fo.f()
+class Person {
+	private _name: string
+	private _age: number
+
+	get age(): number {
+		return this._age
+	}
+
+	set age(y: number) {
+		this._age = y < 0 ? 0 : y
+	}
+
+	get name(): string {
+		return this._name
+	}
+
+	set name(y: string) {
+		this._name = y || this._name
+	}
+
+	constructor(n: string, y: number) {
+		this._name = n
+		this._age = y
+	}
+
+	print(): string {
+		let msg: string = '<p>My name is ' + this._name + '. I am ' + this.age + ' year old.</p>'
+		return msg
 	}
 }
 
-o.a()
-// f 中的 this 會指向 f 所在的物件
-// af 中的 this 會指向 af 所在物件的外層物件
-// f 中的 f 中的 this 會指向 window
-// f 中的 af 中的 this 會指向 f 所在的物件
-// f 中的 af 中的 af 中的 this 會指向 f 所在的物件
-// f 中的 af 中的 f 中的 this 會指向 window
+function doClick(): void {
+	let val: string = (<HTMLInputElement>document.querySelector('#text1')).value
+	let arr: string[] = val.split(',')
 
-// 也就是說
-// 在 o 建立 f，f 內的 this 會變成 o
-// 在 f 建立 f，f 內的 this 會變成 window
-// 在 o 建立 af，af 內的 this 與 o 的 this 相同
-// 在 o 建立 oo，oo 內的 this 與 o 的 this 相同
-
-// Outer Environment 的概念
-// 如果要呼叫一個在該 exection context 中沒有的變項時，它會往它的 outer environment 去找
-
-// 所以
-// 要知道 this 是什麼就往外層找 f
-// this 就是 f 所在的物件 
-// f 在 f or af 裡
-// this 就是 window
-
-// 結論
-// f 有 this 變項
-// o, af 沒有
+	let obj: Person = new Person(arr[0], parseInt(arr[1]))
+	let msg: Element = document.querySelector('#msg')
+	obj.age = -1
+	obj.name = ""
+	msg.innerHTML = obj.print()
+}
