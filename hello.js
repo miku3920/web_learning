bd5b47de1b10f6e52fd22334a22aa2d0b0f1fda9
miku3920 <papa964204@gmail.com>
2020-08-29 19:58:01 +0800

    3-21 extends

--- hello.js
+++ hello.js
@@ -1,40 +1,50 @@
+var __extends = (this && this.__extends) || (function () {
+	var extendStatics = function (d, b) {
+		extendStatics = Object.setPrototypeOf ||
+			({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+			function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+		return extendStatics(d, b);
+	};
+	return function (d, b) {
+		extendStatics(d, b);
+		function __() { this.constructor = d; }
+		d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+	};
+})();
 var Person = /** @class */ (function () {
-    function Person(n, y) {
-        this._name = n;
-        this._age = y;
-    }
-    Object.defineProperty(Person.prototype, "age", {
-        get: function () {
-            return this._age;
-        },
-        set: function (y) {
-            this._age = y < 0 ? 0 : y;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Person.prototype, "name", {
-        get: function () {
-            return this._name;
-        },
-        set: function (y) {
-            this._name = y || this._name;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Person.prototype.print = function () {
-        var msg = '<p>My name is ' + this._name + '. I am ' + this.age + ' year old.</p>';
-        return msg;
-    };
-    return Person;
+	function Person(n, y) {
+		this.name = n;
+		this.age = y;
+	}
+	Person.prototype.print = function () {
+		var msg = '<p>My name is ' + this.name + '. I am ' + this.age + ' year old.</p>';
+		return msg;
+	};
+	return Person;
 }());
+var Student = /** @class */ (function (_super) {
+	__extends(Student, _super);
+	function Student(n, y, g) {
+		var _this = _super.call(this, n, y) || this;
+		_this.grade = g;
+		return _this;
+	}
+	Student.prototype.printAll = function () {
+		var msg = '<p>我是 ' + this.name + '，今年 ' + this.age + ' 歲，現在 ' + this.grade + ' 年級。</p>';
+		return msg;
+	};
+	return Student;
+}(Person));
 function doClick() {
-    var val = document.querySelector('#text1').value;
-    var arr = val.split(',');
-    var obj = new Person(arr[0], parseInt(arr[1]));
-    var msg = document.querySelector('#msg');
-    obj.age = -1;
-    obj.name = "";
-    msg.innerHTML = obj.print();
+	var msg = document.querySelector('#msg');
+	var val = document.querySelector('#text1').value;
+	var arr = val.split(',');
+	if (arr.length == 2) {
+		var obj = new Person(arr[0], parseInt(arr[1]));
+		msg.innerHTML = obj.print();
+	}
+	else if (arr.length == 3) {
+		var obj = new Student(arr[0], parseInt(arr[1]), parseInt(arr[2]));
+		msg.innerHTML = obj.printAll();
+	}
 }
--- hello.ts
+++ hello.ts
@@ -1,41 +1,43 @@
 class Person {
-	private _name: string
-	private _age: number
-
-	get age(): number {
-		return this._age
-	}
-
-	set age(y: number) {
-		this._age = y < 0 ? 0 : y
-	}
-
-	get name(): string {
-		return this._name
-	}
-
-	set name(y: string) {
-		this._name = y || this._name
-	}
+	public name: string
+	public age: number
 
 	constructor(n: string, y: number) {
-		this._name = n
-		this._age = y
+		this.name = n
+		this.age = y
 	}
 
 	print(): string {
-		let msg: string = '<p>My name is ' + this._name + '. I am ' + this.age + ' year old.</p>'
+		let msg: string = '<p>My name is ' + this.name + '. I am ' + this.age + ' year old.</p>'
+		return msg
+	}
+}
+
+class Student extends Person {
+	public grade: number
+
+	constructor(n: string, y: number, g: number) {
+		super(n, y)
+		this.grade = g
+	}
+
+	printAll(): string {
+		let msg: string = '<p>我是 ' + this.name + '，今年 ' + this.age + ' 歲，現在 ' + this.grade + ' 年級。</p>'
 		return msg
 	}
 }
 
 function doClick(): void {
+	let msg: Element = document.querySelector('#msg')
 	let val: string = (<HTMLInputElement>document.querySelector('#text1')).value
+
 	let arr: string[] = val.split(',')
 
-	let obj: Person = new Person(arr[0], parseInt(arr[1]))
-	let msg: Element = document.querySelector('#msg')
-	obj.age = -1
-	obj.name = ""
-	msg.innerHTML = obj.print()
+	if (arr.length == 2) {
+		let obj: Person = new Person(arr[0], parseInt(arr[1]))
+		msg.innerHTML = obj.print()
+	}else if (arr.length == 3) {
+		let obj: Student = new Student(arr[0], parseInt(arr[1]), parseInt(arr[2]))
+		msg.innerHTML = obj.printAll()
+	}
 }
