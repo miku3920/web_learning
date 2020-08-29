18a5ba7170410c95b8ed15016986eeaec114b4d6
miku3920 <papa964204@gmail.com>
2020-08-29 22:53:21 +0800

    3-24 static

--- hello.js
+++ hello.js
@@ -1,16 +1,3 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 var Person = /** @class */ (function () {
     function Person(n, y) {
         this.name = n;
@@ -20,35 +7,14 @@ var Person = /** @class */ (function () {
         var msg = '<p>My name is ' + this.name + '. I am ' + this.age + ' year old.</p>';
         return msg;
     };
+    Person.tag = 'p';
     return Person;
 }());
-var Student = /** @class */ (function (_super) {
-    __extends(Student, _super);
-    function Student(n, y, g) {
-        var _this = _super.call(this, n, y) || this;
-        _this.grade = g;
-        return _this;
-    }
-    Student.prototype.print = function () {
-        var msg = '<p>我是 ' + this.name + '，今年 ' + this.age + ' 歲，現在 ' + this.grade + ' 年級。</p>';
-        return msg;
-    };
-    return Student;
-}(Person));
 function doClick() {
     var msg = document.querySelector('#msg');
     var val = document.querySelector('#text1').value;
     var arr = val.split(',');
-    var obj = null;
-    if (arr.length == 2) {
-        obj = new Person(arr[0], parseInt(arr[1]));
-    }
-    else if (arr.length == 3) {
-        obj = new Student(arr[0], parseInt(arr[1]), parseInt(arr[2]));
-    }
+    var obj = new Person(arr[0], parseInt(arr[1]));
+    Person.tag = 'h1';
     msg.innerHTML = obj.print();
-    for (var prop in obj) {
-        console.log(prop + ': ' + obj[prop]);
-    }
-    console.log(obj);
 }
--- hello.ts
+++ hello.ts
@@ -1,6 +1,7 @@
 class Person {
 	public name: string
 	public age: number
+	public static tag = 'p'
 
 	constructor(n: string, y: number) {
 		this.name = n
@@ -13,34 +14,12 @@ class Person {
 	}
 }
 
-class Student extends Person {
-	public grade: number
-
-	constructor(n: string, y: number, g: number) {
-		super(n, y)
-		this.grade = g
-	}
-
-	print(): string {
-		let msg: string = '<p>我是 ' + this.name + '，今年 ' + this.age + ' 歲，現在 ' + this.grade + ' 年級。</p>'
-		return msg
-	}
-}
-
 function doClick(): void {
 	let msg: Element = document.querySelector('#msg')
 	let val: string = (<HTMLInputElement>document.querySelector('#text1')).value
 
 	let arr: string[] = val.split(',')
-	let obj: Person = null
-	if (arr.length == 2) {
-		obj = new Person(arr[0], parseInt(arr[1]))
-	} else if (arr.length == 3) {
-		obj = new Student(arr[0], parseInt(arr[1]), parseInt(arr[2]))
-	}
+	let obj: Person = new Person(arr[0], parseInt(arr[1]))
+	Person.tag='h1'
 	msg.innerHTML = obj.print()
-	for (var prop in obj) {
-		console.log(prop + ': ' + obj[prop])
-	}
-	console.log(obj)
 }
