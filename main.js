8b23f5ff5394b4d1eeeaa011e768daa7e47bc043
miku3920 <papa964204@gmail.com>
2020-08-31 16:10:29 +0800

    4-8 v-on

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 new Vue({
 	el: '#msg',
 	data: {
-		message: 'this is Vue.js sample!'
+		message: 'this is Vue.js sample!',
+		text1:''
+	},
+	methods: {
+		doAction () {
+			var str = this.text1;
+			this.message = 'you typed: ' + str + '.';
+		}
 	}
 })
