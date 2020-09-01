52d4cbf9dc0dad212cf402289ff457db639a4170
miku3920 <papa964204@gmail.com>
2020-09-02 00:33:06 +0800

    4-36 directive

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
-Vue.component('my-component', {
-	props: ['items'],
-	template: '#my-template'
+Vue.directive('my-directive', {
+	bind(el) {
+		el.innerHTML = '<p>**bind now**</p>';
+	},
+	update(el, binding) {
+		if (binding.oldValue != binding.value) {
+			el.innerHTML = '<p>' + binding.oldValue + ' -> ' + binding.value + '</p>';
+		}
+		console.log(binding.value);
+	},
+	unbind(el) {
+		el.innerHTML = '<p>**unbind**</p>';
+	}
 })
 
 new Vue({
 	el: '#msg',
 	data: {
-		data: []
+		val: '',
+		message: 'this is message!'
+	},
+	methods: {
+		myfunc() {
+			this.message = this.val;
+		}
 	}
 })
