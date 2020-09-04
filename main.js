1468f62aacac3b4b14b2564ecbff94783556e62d
miku3920 <papa964204@gmail.com>
2020-09-04 10:42:19 +0800

    5-7 view object
    backbone.js

--- main.js
+++ main.js
@@ -1,27 +1,13 @@
-Vue.directive('my-directive', {
-	bind(el) {
-		el.innerHTML = '<p>**bind now**</p>';
-	},
-	update(el, binding) {
-		if (binding.oldValue != binding.value) {
-			el.innerHTML = '<p>' + binding.oldValue + ' -> ' + binding.value + '</p>';
-		}
-		console.log(binding.value);
-	},
-	unbind(el) {
-		el.innerHTML = '<p>**unbind**</p>';
-	}
-})
+$(function(){
+	let MyView = Backbone.View.extend({
+		render() {
+			this.$el.text('Hello Backbone.js!');
+			return this;
+		},
+	});
 
-new Vue({
-	el: '#msg',
-	data: {
-		val: '',
-		message: 'this is message!'
-	},
-	methods: {
-		myfunc() {
-			this.message = this.val;
-		}
-	}
-})
+	let myView = new MyView();
+
+	$('#msg').append(myView.render().$el);
+	
+});
