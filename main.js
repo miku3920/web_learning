c880cb0956789c5ac9aef4becdb59e88cbe6558b
miku3920 <papa964204@gmail.com>
2020-09-05 00:32:59 +0800

    install airbnb-base

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-$(function () {
-	let MyTagView = Backbone.View.extend({
+$(() => {
+	const MyTagView = Backbone.View.extend({
 		tagName: 'p',
 		className: 'msg',
 		id() {
 			return _.uniqueId('item');
 		},
 		attributes: {
-			'style': 'color: white; background: red; padding: 5px 10px;'
-		}
+			style: 'color: white; background: red; padding: 5px 10px;',
+		},
 	});
 
-	let myTag = new MyTagView();
+	const myTag = new MyTagView();
 	myTag.el.textContent = '這是新增的標籤。';
-	console.log(myTag);
 	$('#msg').html(myTag.el);
 });
