2177dd31cbe1c6a6b175dbc37196dfff19ffa9a8
miku3920 <papa964204@gmail.com>
2020-09-04 19:47:03 +0800

    5-12 initialize

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 $(function () {
 	let MyView = Backbone.View.extend({
+		initialize() {
+			this.$title = $('#title');
+			this.$item1 = $('#item1');
+			this.$item2 = $('#item2');
+			this.$item3 = $('#item3');
+		},
 		el: '#msg',
-
 		render() {
-			this.$('#title').text('※使用作業系統平台');
-			this.$('#item1').text('Windows');
-			this.$('#item2').text('macOS');
-			this.$('#item3').text('Linux');
+			this.$title.text('※使用作業系統平台');
+			this.$item1.text('Windows');
+			this.$item2.text('macOS');
+			this.$item3.text('Linux');
 			return this;
 		},
 	});
