307131e9f1b64b52e562edac8754f7c6cf5eb04a
miku3920 <papa964204@gmail.com>
2020-09-07 17:29:00 +0800

    5-21 mutiple view

--- main.js
+++ main.js
@@ -1,16 +1,45 @@
 $(() => {
+	// 顯示訊息用的 View
 	const MyView = Backbone.View.extend({
 		el: '#msg',
-		tmp1: _.template($('#myview-template').html()),
-		render() {
-			this.$el.html(this.tmp1({
-				title: '山田太郎',
-				content: '任職ＯＯ銀行<br>e-mail: taro@yamada',
-			}));
-			return this;
-		},
 	});
 
 	const myView = new MyView();
-	myView.render();
+
+	// 輸入欄位的 View
+	const InputView = Backbone.View.extend({
+		events: {
+			keypress: 'onkeydown',
+		},
+		onkeydown(event) {
+			this.value = this.$el.val();
+		},
+		el: '#input1',
+	});
+
+	const inputView = new InputView();
+
+	// 按鈕的 View
+	const ButtonView = Backbone.View.extend({
+		events: {
+			click: 'onclick',
+		},
+		initialize(obj) {
+			this.input = obj.input;
+			this.msg = obj.msg;
+			this.$el.val('click');
+		},
+		onclick(event) {
+			const str = this.input.$el.val();
+			this.msg.$el.text(`typed: ${str}`);
+		},
+		el: '#btn1',
+		input: null,
+		msg: null,
+	});
+
+	const buttonView = new ButtonView({
+		input: inputView,
+		msg: myView,
+	});
 });
