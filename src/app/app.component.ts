ff37ae191456e21f298ad7300fd2d5782a6e8428
miku3920 <papa964204@gmail.com>
2020-09-10 19:53:01 +0800

    6-10 mutiple component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-	selector: 'app-root',
+	selector: 'app-1',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css'],
 })
@@ -9,4 +9,12 @@ export class AppComponent {
 	title = 'Hello!';
 
 	message = '這是 Angular 的範例程式。';
+}
+
+@Component({
+	selector: 'app-2',
+	template: '<h1>{{title}}</h1>',
+})
+export class OtherComponent {
+	title = 'Second Component!';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent, OtherComponent } from './app.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
+		OtherComponent,
 	],
 	imports: [
 		BrowserModule,
 	],
 	providers: [],
-	bootstrap: [AppComponent],
+	bootstrap: [AppComponent, OtherComponent],
 })
 export class AppModule { }
