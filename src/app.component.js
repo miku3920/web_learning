85310385afadfb576887867eb04baf45d27cfdce
miku3920 <papa964204@gmail.com>
2020-09-20 13:30:13 +0800

    7-15 state

--- src/app.component.js
+++ src/app.component.js
@@ -1,19 +1,33 @@
-import PropTypes from 'prop-types';
-import React, { useCallback } from 'react';
+import React from 'react';
 
-function Component({ msg }, dispatch) {
-	const handleOnClick = useCallback(() => {
-		alert('已點選！');
-	}, []);
-	return (
-		<div>
-			<p>{msg}</p>
-			<input type="button" value="click" onClick={handleOnClick} />
-		</div>
-	);
+class Component extends React.Component {
+	constructor() {
+		super();
+		this.displayName = 'component';
+		this.state = { msg: '請輸入名字：' };
+		this.handleOnChange = this.handleOnChange.bind(this);
+		this.handleOnClick = this.handleOnClick.bind(this);
+	}
+
+	handleOnChange(event) {
+		console.log(event.target.value);
+		this.inputValue = event.target.value;
+	}
+
+	handleOnClick() {
+		this.setState({
+			msg: `你好！${this.inputValue}先生/小姐！`,
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<p>{this.state.msg}</p>
+				<input type="text" onChange={this.handleOnChange} />
+				<input type="button" value="click" onClick={this.handleOnClick} />
+			</div>
+		);
+	}
 }
-Component.displayName = 'component';
-Component.propTypes = {
-	msg: PropTypes.string.isRequired,
-};
 export default Component;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 import Component from './app.component';
 
 ReactDOM.render(
-	<Component msg="請點選下方按紐。" />,
+	<Component />,
 	document.getElementById('msg'),
 );
